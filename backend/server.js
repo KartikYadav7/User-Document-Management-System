65126ac03dfae1d24a139b97bc6c2c615b87b3b2
kartikyadav7 <raokartik7503@gmail.com>
2025-12-11 12:58:35 +0530

    frontend url updated

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-    origin: ['http://localhost:5173',],
+    origin: ['http://localhost:5173','https://user-doc-sys.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }))
